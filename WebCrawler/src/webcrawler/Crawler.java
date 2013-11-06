16a6076a60dbc378326a7e0a573d7a28ac12908d
motimz <motimz@hotmail.com>
2013-11-06 15:54:51 +0200

    crawler interface defined

--- WebCrawler/src/webcrawler/Crawler.java
+++ WebCrawler/src/webcrawler/Crawler.java
@@ -4,10 +4,30 @@
  */
 package webcrawler;
 
+import java.util.Queue;
+
 /**
  *
  * @author motimi
  */
 public class Crawler {
+    
+    private Queue<MyUrl> _urls;
+    private Validator _validator;
+    
+    public Crawler(Validator v)
+    {
+        _validator=v;
+    }
+    
+    public void startCrawl(MyUrl url,int limit)
+    {
+        
+    }
+    
+    public void scanUrl(MyUrl url)
+    {
+        
+    }
     
 }
