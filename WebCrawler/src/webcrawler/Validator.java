a9beadbe839ce6b8d5285f0e642eeec9a8fe0532
Gil Mizrahi <suppiral@yahoo.com>
2013-11-06 16:04:45 +0200

    Validator interface declared

--- WebCrawler/src/webcrawler/Validator.java
+++ WebCrawler/src/webcrawler/Validator.java
@@ -4,10 +4,27 @@
  */
 package webcrawler;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  *
  * @author gilmi
+ * 
  */
 public class Validator {
+    private Set<MyUrl> _visited = new HashSet();
+    private String _keywords;
+    private String _fileType;
+    
+    /** Constructor for Validator class
+     * @param keywords that needs to be in the title
+     */
+    public Validator(String keywords) 
+    {
+        
+    }
+    /** STUB Validate Function */
+    public Boolean Validate(MyUrl url) { return true; }
     
 }
