8d2cdc1b9b6cd0030198712502fbf16296cd0c48
Gil Mizrahi <suppiral@yahoo.com>
2013-11-08 14:39:54 +0200

    added validation and MyUrl changes
    validation: added a validation function that is yet to be checked.
    MyUrl: added a few functions to help handling it

--- WebCrawler/src/webcrawler/Validator.java
+++ WebCrawler/src/webcrawler/Validator.java
@@ -4,9 +4,12 @@
  */
 package webcrawler;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -16,16 +19,100 @@ import java.util.Set;
 public class Validator {
     private Set<MyUrl> _visited = new HashSet();
     private String _keywords;
-    private String _fileType;
+    private static final String FILETYPE = "text/html";
     
     /** Constructor for Validator class
      * @param keywords that needs to be in the title
      */
     public Validator(ArrayList<String> keywords) 
     {
+        // add keywords to pattern string
+        _keywords += "(";
+        for (String keyword : keywords)
+           _keywords += keyword + "|";
+        
+        // remove last "|" and replace it with ")"
+        _keywords.substring(0, _keywords.length()-1);
+        if (_keywords.length() != 0)
+            _keywords += ")";
         
     }
-    /** STUB Validate Function */
-    public Boolean Validate(MyUrl url) { return true; }
+    /** Validate Function. checks 4 things:
+     * 1. have we seen this url?
+     * 2. is it disallowed?
+     * 3. is it the wrong filetype?
+     * 4. it none of the keywords specified are in it's title?
+     * 
+     * if it answers "false" on one of these questions, it returns false
+     * otherwise, it returns true
+     * 
+     * @param url 
+     * @param robotsFile 
+     * @return Boolean 
+     */
+    public Boolean Validate(MyUrl url, MyUrl robotsFile) 
+    {
+        if (isVisited(url))
+            return false;
+        
+        if (isDisallowed(url, robotsFile))
+            return false;
+        
+        if (!isFileType(url))
+            return false;
+        
+        return hasKeywordInTitle(url);
+    }
     
+    private Boolean isVisited(MyUrl url)
+    {
+        if (_visited.contains(url))
+            return true;
+        
+        _visited.add(url);
+        return false;
+         
+    }
+    
+    private static Boolean isDisallowed(MyUrl url, MyUrl robots)
+    {
+        try {    
+           String robo = robots.getString();
+           String pattern = "Disallow:\\s*" + url.getPath();
+           Matcher matcher = Pattern.compile(pattern).matcher(robo);
+           
+           if (matcher.find())
+               return true;
+        }
+        catch (IOException e) {} // no robots? no problem.
+        
+        return false;
+    }
+    
+    private static Boolean isFileType(MyUrl url)
+    {
+        try
+        {
+            if (url.getType().equalsIgnoreCase(FILETYPE))
+                return true;
+        } catch (IOException e) { }
+        
+        return false;
+    }
+    private Boolean hasKeywordInTitle(MyUrl url)
+    {
+        try 
+        {
+            String pattern = "<title>(" + _keywords + ")<title>";
+            Matcher matcher = Pattern.
+                   compile(pattern, Pattern.CASE_INSENSITIVE).
+                   matcher(url.getString());
+           
+           if (matcher.find())
+               return true;
+        }
+        catch (IOException e) {}
+        
+        return false;
+    }
 }
