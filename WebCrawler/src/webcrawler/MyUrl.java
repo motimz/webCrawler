cc47c97e76779e30345853fafa36269e657b411c
Gil Mizrahi <suppiral@yahoo.com>
2013-11-13 11:40:36 +0200

    added documentation.

--- WebCrawler/src/webcrawler/MyUrl.java
+++ WebCrawler/src/webcrawler/MyUrl.java
@@ -78,6 +78,8 @@ public class MyUrl
         
          String type = connection.getContentType();
         connection.disconnect();
+        if (type == null)
+            type = "";
         
          return type;
     }
